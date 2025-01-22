73971746f95a50f35097ae626fd762ab374a1373
sehrishmali <sehrishmali786@gmail.com>
2025-01-22 11:09:07 -0800

    first commit

---
+++ src/app/footer/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const 
+Footer = () => {
+  return (
+    <div>
+        Footer page
+    </div>
+  )
+}
+
+export default Footer 
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import Home from "./page";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
